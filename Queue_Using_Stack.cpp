04438f543851eb62e9cc631d72576bd48aa5e924
Akash Suklabaidya <akashsuklabaidya593@gmail.com>
2023-06-21 13:58:36 +0530

    Day 12

--- Queue_Using_Stack.cpp
+++ Queue_Using_Stack.cpp
@@ -1,85 +1,20 @@
-// #include <bits/stdc++.h>
-// class Queue
-// {
-//     // Define the data members(if any) here.
-
-// public:
-//     stack<int> st1, st2;
-//     Queue()
-//     {
-//     }
-
-//     void enQueue(int val)
-//     {
-//         st1.push(val);
-//     }
-
-//     int deQueue()
-//     {
-//         if (st1.size() == 0)
-//         {
-//             return -1;
-//         }
-//         while (st1.size() > 1)
-//         {
-//             st2.push(st1.top());
-//             st1.pop();
-//         }
-//         int ans = st1.top();
-//         st1.pop();
-//         while (!st2.empty())
-//         {
-//             st1.push(st2.top());
-//             st2.pop();
-//         }
-//         return ans;
-//     }
-
-//     int peek()
-//     {
-//         if (st1.size() == 0)
-//         {
-//             return -1;
-//         }
-//         while (st1.size() > 1)
-//         {
-//             st2.push(st1.top());
-//             st1.pop();
-//         }
-//         int ans = st1.top();
-//         while (!st2.empty())
-//         {
-//             st1.push(st2.top());
-//             st2.pop();
-//         }
-//         return ans;
-//     }
-
-//     bool isEmpty()
-//     {
-//         if (st1.size() == 0)
-//         {
-//             return true;
-//         }
-//         return false;
-//     }
-// };
-
-class MyQueue
+#include <bits/stdc++.h>
+class Queue
 {
+    // Define the data members(if any) here.
+
 public:
     stack<int> st1, st2;
-
-    MyQueue()
+    Queue()
     {
     }
 
-    void push(int x)
+    void enQueue(int val)
     {
-        st1.push(x);
+        st1.push(val);
     }
 
-    int pop()
+    int deQueue()
     {
         if (st1.size() == 0)
         {
@@ -120,7 +55,7 @@ public:
         return ans;
     }
 
-    bool empty()
+    bool isEmpty()
     {
         if (st1.size() == 0)
         {
@@ -129,12 +64,3 @@ public:
         return false;
     }
 };
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue* obj = new MyQueue();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->peek();
- * bool param_4 = obj->empty();
- */
